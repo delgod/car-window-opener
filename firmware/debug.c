c90e94e77895c60abbe787f874e53b8c7ee8cea8
Mykola Marzhan <Mykola.Marzhan@portaone.com>
2015-02-10 10:44:15 +0200

    sync debug.c and main.c code

--- firmware/debug.c
+++ firmware/debug.c
@@ -1,17 +1,16 @@
 #include <avr/io.h>
 
-#define ENG_ON         PORTB |= _BV(PB4)         // enable engine
-#define ENG_OFF        PORTB &= ~_BV(PB4)        // disable engine
-#define BUTTON_STATE  bit_is_set( PINB, PB3 )   // is button pressed?
+#define ENG_OFF       PORTB |= 0b00010000        // disable engine
+#define ENG_ON        PORTB &= 0b11101111        // enable engine
+#define BUTTON_STATE  bit_is_set( PINB, PB3 )    // is button pressed?
 
 int main(void) {
+ 
+    // set PB3 as input, other pins as output
+    DDRB = 0b11110111;
 
-    // output pin
-    DDRB |= _BV(PB4);     // LED on pin 3
-
-    // input pin
-    DDRB &= ~_BV(PB3);    // switch on pin 2
-    PORTB |= _BV(PB3);    // enable pull-up resistor
+    // engine off by default
+    ENG_OFF;
 
     while (1) {
         if (BUTTON_STATE) {    // button is pressed now
